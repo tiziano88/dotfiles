72d26b00c7638eab3a02dd6571ff157774828530
Tiziano Santoro <tzn@google.com>
2013-03-27 21:40:48 +0000

    offlineimap

--- .offlineimap.py
+++ .offlineimap.py
@@ -2,5 +2,11 @@
 
 import keyring
 import sys
+import os
+import subprocess
 
-print('--')
+def get_user(account):
+  return subprocess.check_output(["echo $PWSAFEPASSWORD | pwsafe -E -q -u " + account], shell=True).strip()
+
+def get_password(account):
+  return subprocess.check_output(["echo $PWSAFEPASSWORD | pwsafe -E -q -p " + account], shell=True).strip()
