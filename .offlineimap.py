8a8fb5e85ce74baf37b69636fdad4d2f5bb71056
Tiziano Santoro <tzn@google.com>
2013-03-28 00:15:28 +0000

    WIP

--- .offlineimap.py
+++ .offlineimap.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python
 
-import keyring
+#import keyring
 import sys
 import os
 import subprocess
 
 def get_user(account):
-  return subprocess.check_output(["echo $PWSAFEPASSWORD | pwsafe -E -q -u " + account + " | tail -n 1"], shell=True).strip()
+  return subprocess.check_output(["echo $PWSAFEPASSWORD | pwsafe -E -q -u " + account + " | sed -n '/^[^ ]*$/p'"], shell=True).strip()
 
 def get_password(account):
-  return subprocess.check_output(["echo $PWSAFEPASSWORD | pwsafe -E -q -p " + account + " | tail -n 1"], shell=True).strip()
+  return subprocess.check_output(["echo $PWSAFEPASSWORD | pwsafe -E -q -p " + account + " | sed -n '/^[^ ]*$/p'"], shell=True).strip()
