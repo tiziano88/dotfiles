286f7ec0c9c4c82065a913cba9af557287617546
Tiziano Santoro <tzn@google.com>
2013-04-02 09:40:55 +0100

    offlineimap

--- .offlineimap.py
+++ .offlineimap.py
@@ -6,8 +6,10 @@ import subprocess
 
 def get_user(account):
   ret = subprocess.check_output(["echo $PWSAFEPASSWORD | pwsafe -E -q -u " + account + " | sed -n '/^[^ ]*$/p'"], shell=True).strip()
+  print("user: " + ret)
   return ret
 
 def get_password(account):
   ret = subprocess.check_output(["echo $PWSAFEPASSWORD | pwsafe -E -q -p " + account + " | sed -n '/^[^ ]*$/p'"], shell=True).strip()
+  print("pass: " + ret)
   return ret
