e42fbfcd621133462d1df7a5bb12e07c7a748b5e
Tiziano Santoro <tzn@google.com>
2013-04-13 01:20:08 +0100

    offlineimap

--- offlineimap.py
+++ offlineimap.py
@@ -15,10 +15,16 @@ def get_password(account):
   print("pass: " + ret)
   return ret
 
+# To create password file:
+def set_password(account, password):
+  account = os.path.basename(account)
+  path = "/usr/local/google/home/tzn/.passwd/%s.gpg" % account
+  args = ["echo", password, "|", "gpg", "--use-agent", "--quiet", "--batch", "-e", path]
+  return subprocess.check_output(args).strip()
+
 def mailpassword(account):
   account = os.path.basename(account)
-  home = os.getenv("HOME")
-  path = "%s/.passwd/%s.gpg" % (home, account)
+  path = "/usr/local/google/home/tzn/.passwd/%s.gpg" % account
   args = ["gpg", "--use-agent", "--quiet", "--batch", "-d", path]
   try:
     return subprocess.check_output(args).strip()
