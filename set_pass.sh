3824e0c893b86a230842033dcd7fe14e9449e237
Tiziano Santoro <tzn@google.com>
2014-04-21 01:27:49 +0100

    cleanup

--- set_pass.sh
+++ set_pass.sh
@@ -1,5 +1,3 @@
 ACCOUNT=$1
 PASS=$2
 exec echo "$PASS" | gpg --use-agent -e > ~/.passwd/$ACCOUNT.gpg
-#path = "/usr/local/google/home/tzn/.passwd/%s.gpg" % account
-#  args = ["gpg", "--use-agent", "--quiet", "--batch", "-d", path]
