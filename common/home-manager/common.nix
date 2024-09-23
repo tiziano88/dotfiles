3fa91cc8fdd2dd1d46b358e97ada70be379715b7
Tiziano Santoro <tiziano88@gmail.com>
2024-09-23 15:00:52 -0700

    Update deps

--- common/home-manager/common.nix
+++ common/home-manager/common.nix
@@ -198,6 +198,15 @@ in
       background = "dark";
       display = "side-by-side";
     };
+    aliases = {
+      st = "status";
+      co = "checkout";
+      br = "branch";
+      ci = "commit";
+      di = "diff";
+      lg = "log --graph --abbrev-commit --decorate --date=relative --format=format:'%C(bold blue)%h%C(reset) - %C(bold green)(%ar)%C(reset) %C(white)%s%C(reset) %C(dim white)- %an%C(reset)%C(bold yellow)%d%C(reset)' --all";
+      cl = "!f() { git push origin HEAD:refs/for/\${1:-main}; }; f";
+    };
   };
 
   programs.go = {
